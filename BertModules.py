c0c9574f9b763005e4fd6ae0145fa29d8db521d3
stupidHIGH <1370215371@qq.com>
2021-05-27 08:19:15 +0800

    load pre-trained BERT ckpt

--- BertModules.py
+++ BertModules.py
@@ -1,16 +1,15 @@
 import torch
 import torch.nn as nn
 from pytorch_transformers import BertModel
-
-
+    
 class BertClassifier(nn.Module):
-
-    def __init__(self, config):
+    def __init__(self, config,model_path):
         super(BertClassifier, self).__init__()
         # Binary classification problem (num_labels = 2)
         self.num_labels = config.num_labels
         # Pre-trained BERT model
-        self.bert = BertModel(config)
+        #self.bert = BertModel(config)
+        self.bert = BertModel.from_pretrained(model_path)
         # Dropout to avoid overfitting
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
         # A single layer classifier added on top of BERT to fine tune for binary classification
@@ -21,11 +20,11 @@ class BertClassifier(nn.Module):
     def forward(self, input_ids, token_type_ids=None, attention_mask=None,
                 position_ids=None, head_mask=None):
         # Forward pass through pre-trained BERT
-        outputs = self.bert(input_ids, position_ids=position_ids, token_type_ids=token_type_ids,
-                            attention_mask=attention_mask, head_mask=head_mask)
+        outputs = self.bert(input_ids, position_ids=position_ids, token_type_ids=token_type_ids,attention_mask=attention_mask, head_mask=head_mask)
 
         # Last layer output (Total 12 layers)
         pooled_output = outputs[-1]
 
         pooled_output = self.dropout(pooled_output)
         return self.classifier(pooled_output)
+                                  
